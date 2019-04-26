e9c3a5034eb0a48078396729aab3eb157cf7bb79
Saulo <saulojoabps@gmail.com>
2019-04-26 03:47:28 -0300

    exemplo em python

---
+++ exemploEmPython.py
@@ -0,0 +1,28 @@
+#coding: utf-8
+import requests
+
+def Frases():
+    get = requests.get("http://allugofrases.herokuapp.com/frases");
+    return get.json();
+
+def fraseID(id):
+    get = requests.get("http://allugofrases.herokuapp.com/fraseid?id=" + id);
+    return get.json();
+
+def procurarFrase(frase):
+    data = {"frase": frase}
+    get = requests.post(url="http://allugofrases.herokuapp.com/procurarFrase", json=data, headers={'Content-Type': "application/json", 'Accept': "application/json"});
+    return get.json();
+
+def frasesPorAutor(autor):
+    data = {"autor": autor}
+    get = requests.post(url="http://allugofrases.herokuapp.com/frasesPorAutor", json=data, headers={'Content-Type': "application/json", 'Accept': "application/json"});
+    return get.json();
+
+def frasesPorLivro(livro):
+    data = {"livro": livro}
+    get = requests.post(url="http://allugofrases.herokuapp.com/frasesPorLivro", json=data, headers={'Content-Type': "application/json", 'Accept': "application/json"});
+    return get.json();
+
+if (__name__ == "__main__"):
+    print("Verifique as funções de exemplo do código :)");
--- teste.py
+++
@@ -1,8 +0,0 @@
-#coding: utf-8
-import requests
-
-data = {"frase": "forte"}
-get = requests.post(url="http://allugofrases.herokuapp.com/procurarFrase", json=data, headers={'Content-Type': "application/json", 'Accept': "application/json"});
-data = get.json();
-
-print(data);
