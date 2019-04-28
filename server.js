171d6cf54cca68e4757860750a954c107a54d56c
Saulo Joab <37988252+saulojoab@users.noreply.github.com>
2019-04-28 18:55:55 -0300

    cors

--- server.js
+++ server.js
@@ -18,16 +18,19 @@ const listener = app.listen(process.env.PORT, function() {
 
 app.get('/frases', function (req, res) 
 {
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.send(search.getAll());
 });
 
 app.get('/fraseAleatoria', function (req, res) 
 {
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.send(search.getRandom());
 });
 
 app.get('/fraseid', function (req, res)
 {
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.send(search.findByID(parseInt(req.query.id)));
 });
 
@@ -35,6 +38,7 @@ app.post('/procurarFrase', function (req, res)
 {
     console.log("Buscando por Frase!");
     console.log("Frase procurada: " + req.body.frase);
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.send(search.findByPhrase(req.body.frase));
 });
 
@@ -42,6 +46,7 @@ app.post('/frasesPorAutor', function (req, res)
 {
     console.log("Buscando por Autor!");
     console.log("Autor procurado: " + req.body.autor);
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.send(search.findByAuthor(req.body.autor));
 });
 
@@ -49,5 +54,6 @@ app.post('/frasesPorLivro', function (req, res)
 {
     console.log("Buscando por Livro!");
     console.log("Livro procurado: " + req.body.livro);
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.send(search.findByBook(req.body.livro));
 });
