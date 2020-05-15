4d72f40a2fdc4b0656e2a1cc2e33635434521914
saulojoab <saulojoabps@gmail.com>
2020-05-15 05:01:24 -0300

    updating python examples with new docs

--- exemplos/python.py
+++ exemplos/python.py
@@ -2,31 +2,31 @@
 import requests
 
 def Frases():
-    get = requests.get("http://allugofrases.herokuapp.com/frases");
-    return get.json();
+    get = requests.get("http://allugofrases.herokuapp.com/frases")
+    return get.json()
 
 def fraseAleatoria():
-    get = requests.get("http://allugofrases.herokuapp.com/fraseAleatoria");
-    return get.json();
+    get = requests.get("http://allugofrases.herokuapp.com/frases/random")
+    return get.json()
 
 def fraseID(id):
-    get = requests.get("http://allugofrases.herokuapp.com/fraseid?id=" + id);
-    return get.json();
+    get = requests.get("http://allugofrases.herokuapp.com/frases/find/" + id)
+    return get.json()
 
 def procurarFrase(frase):
     data = {"frase": frase}
-    get = requests.post(url="http://allugofrases.herokuapp.com/procurarFrase", json=data, headers={'Content-Type': "application/json", 'Accept': "application/json"});
-    return get.json();
+    get = requests.post(url="http://allugofrases.herokuapp.com/frases/search", json=data, headers={'Content-Type': "application/json", 'Accept': "application/json"})
+    return get.json()
 
 def frasesPorAutor(autor):
     data = {"autor": autor}
-    get = requests.post(url="http://allugofrases.herokuapp.com/frasesPorAutor", json=data, headers={'Content-Type': "application/json", 'Accept': "application/json"});
-    return get.json();
+    get = requests.post(url="http://allugofrases.herokuapp.com/frases/author", json=data, headers={'Content-Type': "application/json", 'Accept': "application/json"})
+    return get.json()
 
 def frasesPorLivro(livro):
     data = {"livro": livro}
-    get = requests.post(url="http://allugofrases.herokuapp.com/frasesPorLivro", json=data, headers={'Content-Type': "application/json", 'Accept': "application/json"});
-    return get.json();
+    get = requests.post(url="http://allugofrases.herokuapp.com/frases/book", json=data, headers={'Content-Type': "application/json", 'Accept': "application/json"})
+    return get.json()
 
 if (__name__ == "__main__"):
-    print("Verifique as funções de exemplo do código :)");
+    print("Verifique as funções de exemplo do código :)")
